6f593a674d2c6410ba30ca20f877c2a7cfc08c2a
Abbas ELMAS <abbaselmas@gmail.com>
2013-05-04 21:59:38 +0300

    Upload project
    partial upload

---
+++ Lexical Analyser/Lexical Analyser/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lexical_Analyser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(richTextBox1 != null)
+            {
+                string a = richTextBox1.Text.Trim();
+                string[] satir = a.Split('\n');
+
+            }
+            else
+            {
+            }
+        }
+
+    }
+}
---
+++ Lexical Analyser/Lexical Analyser/lexgram.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lexical_Analyser
+{
+    class lexgram
+    {
+        private static short lof_ident = 10;
+        bool check()
+        {
+            return true;
+        }
+
+        /***********************/
+
+        void s_input()
+        {
+            s_main(); /* | */s_stmt();
+        }
+        void s_main()
+        {
+            /* program */ s_identifier(); s_stmt();/* [ */ s_stmt(); /* ] end */
+        }
+        void s_stmt()
+        {
+            s_assign(); /* | */ s_if_stmt(); /* | */ s_while_stmt();
+        }
+        void s_assign()
+        {
+            s_identifier(); /* := */ s_expr();
+        }
+        void s_if_stmt()
+        {
+            /* if( */ s_cond(); /* )then */ s_stmt(); /* [else */ s_stmt(); /* ] */
+        }
+        void s_while_stmt()
+        {
+            /* while( */ s_cond(); /* ) */ s_block();
+        }
+        void s_cond()
+        {
+            s_identifier(); /* >,<,>=,<=,<> */ s_identifier();
+        }
+        void s_block()
+        {
+            /* begin */ s_stmt(); /* end */
+        }
+        void s_expr()
+        {
+            s_expr(); /* (+ , -  , * ,  / )  */ s_expr();
+            /* |( */ s_expr(); /* ) */
+            /* | */ s_identifier();
+            /* | */ s_int_value();
+        }
+        void s_identifier()
+        {
+            /* A|…|Z|a|…|z [A|…|Z|a|…|z|0|…|9] */
+        }
+        void s_int_value()
+        {
+            /* any legal integer number in C# */
+        }
+    }
+}
